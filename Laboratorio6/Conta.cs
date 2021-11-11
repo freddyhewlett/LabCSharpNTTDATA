432e0d3964bdddbcae4099d6dca3de4d30ec43e9
Federico Hewlett <farcinih@nttdata.com>
2021-11-11 14:18:55 -0300

    Laboratorio 6 em andamento

---
+++ Laboratorio6/Conta.cs
@@ -0,0 +1,41 @@
+﻿
+namespace Laboratorio6
+{
+    public abstract class Conta
+    {
+        private decimal saldo;
+        private string titular;
+        private string tipoConta;
+
+        public Conta (string nom, string tc)
+        {
+            titular = nom;
+            tipoConta = tc;
+        }
+
+        public decimal Saldo
+        {
+            get { return saldo; }
+        }
+
+        public string Titular
+        {
+            get { return titular; }
+        }
+
+        public abstract string Id
+        {
+            get;
+        }
+
+        public virtual void Depositar(decimal valor)
+        {
+            saldo += valor;
+        }
+
+        public virtual void Sacar(decimal valor)
+        {
+            saldo -= valor;
+        }
+    }
+}
---
+++ Laboratorio6/ContaPoupanca.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Laboratorio6
+{
+    public class ContaPoupanca : Conta
+    {
+        private decimal taxaJuros;
+        private DateTime dataAniversario;
+
+        public ContaPoupanca(decimal j, DateTime d, string n, string tc) : base(n, tc)
+        {
+            taxaJuros = j;
+            dataAniversario = d;
+        }
+
+        public decimal Juros
+        {
+            get { return taxaJuros; }
+            set { taxaJuros = value; }
+        }
+
+        public DateTime DataAniversario
+        {
+            get { return dataAniversario; }
+        }
+
+        public void AdicionarRendimento()
+        {
+            DateTime hoje = DateTime.Now;
+            if (hoje.Day == dataAniversario.Day && hoje.Month == dataAniversario.Month)
+            {
+                decimal rendimento = this.Saldo * taxaJuros;
+                this.Depositar(rendimento);
+            }
+        }
+
+        public override string Id
+        {
+            get { return this.Titular + "(CP)"; }
+        }
+    }
+}
